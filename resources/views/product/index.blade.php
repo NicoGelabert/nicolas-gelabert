230e35e1fbb30fe36a564dc0b5fdba7c3222411c
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-09-02 21:02:52 +0200

    Small fix in product listing

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -40,24 +40,24 @@
                                 <h5 class="font-number pl-4 text-lg md:text-xl lg:text-2xl">${{$product->price}}</h5>
                             </div>
                         </div>
-                        <button class="btn-primary absolute btn-cart-product" @click="addToCart()">
-                            <!-- Add to Cart -->
-                            <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="1"
-                            >
-                                <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                />
-                            </svg>
-                        </button>
                     </a>
+                    <button class="btn-primary absolute btn-cart-product" @click="addToCart()">
+                        <!-- Add to Cart -->
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="1"
+                        >
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                            />
+                        </svg>
+                    </button>
                 </div>
                 <!--/ Product Item -->
             @endforeach
