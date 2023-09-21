1ab5f759835d7b7c1182f6e1ccee0ce6902293ff
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-09-21 18:08:58 +0200

    Promo section

---
+++ resources/views/components/promo-welcome.blade.php
@@ -0,0 +1,44 @@
+<div class="flex flex-col sm:flex-row relative gap-16 -mx-5 md:mx-0">
+    <!-- text -->
+    <div class="w-full md:w-1/2 flex flex-col justify-center bg-[url('https://images.pexels.com/photos/1983036/pexels-photo-1983036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] bg-cover md:bg-none text-center md:text-left">
+        <div class="bg-black/30 p-8 md:p-0 md:bg-transparent">
+            <h2 class="text-3xl font-bold tracking-tight text-white md:text-gray-900 sm:text-4xl md:text-6xl drop-shadow-lg md:drop-shadow-none">{{__('Get vintage. Get real.')}}</h2>
+            <p class="my-8 text-xl text-white md:text-gray-500 drop-shadow-lg md:drop-shadow-none">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="w-auto">
+                <a href="#" class="btn-primary">Shop Collection</a>
+            </div>
+        </div>
+    </div>
+    <!-- text -->
+    <!-- image -->
+    <div class="hidden md:w-1/2 md:flex items-center gap-3 md:gap-6">
+        <div class="  w-1/3 gap-y-4 flex flex-col">
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/1983036/pexels-photo-1983036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="h-full w-full object-cover object-center">
+            </div>
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/8086616/pexels-photo-8086616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="h-full w-full object-cover object-center">
+            </div>
+        </div>
+        <div class=" w-1/3 gap-y-4 flex flex-col">
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/3144800/pexels-photo-3144800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="h-full w-full object-cover object-center">
+            </div>
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/3907509/pexels-photo-3907509.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" class="h-full w-full object-cover object-center">
+            </div>
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/3781267/pexels-photo-3781267.jpeg" alt="" class="h-full w-full object-cover object-center">
+            </div>
+        </div>
+        <div class=" w-1/3 gap-y-4 flex flex-col">
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/2910207/pexels-photo-2910207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="h-full w-full object-cover object-center">
+            </div>
+            <div class="overflow-hidden rounded-lg">
+                <img src="https://images.pexels.com/photos/3117811/pexels-photo-3117811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="h-full w-full object-cover object-center">
+            </div>
+        </div>
+    </div>
+    <!-- image -->
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,30 +2,26 @@
     /** @var \Illuminate\Database\Eloquent\Collection $products */
     ?>
     <x-app-layout>
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+    <div class="relative isolate px-6 py-24 lg:px-8">
+        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            {{__('Site design and developed by')}} Nicolás Gelabert 
+                <a href="/" class="font-semibold text-indigo-600">
+                    <span class="absolute inset-0" aria-hidden="true"></span>{{__('Back to CV')}} <span aria-hidden="true">&rarr;</span>
+                </a>
             </div>
         </div>
-        <div class="mx-auto max-w-2xl py-4">
-            <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                {{__('Site design and developed by')}} Nicolás Gelabert 
-                    <a href="/" class="font-semibold text-indigo-600">
-                        <span class="absolute inset-0" aria-hidden="true"></span>{{__('Back to CV')}} <span aria-hidden="true">&rarr;</span>
-                    </a>
-                </div>
-            </div>
-            <div class="text-center">
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">E-Commerce demo</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">{{__('E-Commerce site development carried out with Laravel, Vue Js and Tailwind.')}}</p>
-                <div class="mt-10 flex items-center justify-center gap-x-6">
-                    <a href="{{ route('product.index') }}" class="btn-primary">{{__('See products')}}</a>
-                    <a href="{{ route('cart.index') }}" class="text-sm font-semibold leading-6 text-gray-900">{{__('Go to cart')}}<span aria-hidden="true">→</span></a>
-                </div>
+        <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">E-Commerce demo</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">{{__('E-Commerce site development carried out with Laravel, Vue Js and Tailwind.')}}</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+                <a href="{{ route('login') }}" class="btn-primary">{{__('Login')}}</a>
+                <a href="{{ route('register') }}" class="text-sm font-semibold leading-6 text-gray-900">{{__('Register now')}}<span aria-hidden="true">→</span></a>
             </div>
         </div>
     </div>
+
+    <x-promo-welcome />
 
     <section id="image-carousel" class="splide my-16 md:mx-16" aria-label="Beautiful Images">
         <div class="mb-8 text-center">
