720ed46a683bc7fbaea2f85091b8e1fb667cc3d5
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-09-29 17:53:44 +0200

    Subcribe section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
     /** @var \Illuminate\Database\Eloquent\Collection $products */
     ?>
     <x-app-layout>
-        <div class="flex items-center h-screen">
-            <div class="w-full md:w-3/5 relative isolate px-6 py-12 lg:px-8 slide-in-left">
+        <div class="flex flex-col md:flex-row items-center relative md:h-screen">
+            <div class="w-full md:w-3/5 relative isolate px-6 pt-24 pb-3 lg:px-8 slide-in-left">
                 <div class="flex mb-4 justify-start">
                     <div class="relative rounded-full sm:px-3 py-1 text-xs lg:text-sm leading-6 text-gray-600 sm:ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                     {{__('Site design and developed by')}} Nicolás Gelabert 
@@ -15,48 +15,50 @@
                 <div class="text-left">
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">E-Commerce demo</h1>
                     <p class="mt-2 text-lg leading-8 text-gray-600">{{__('E-commerce site developed with Laravel, Vue JS and Tailwind.')}}</p>
-                    <div class="mt-4 flex items-center justify-start gap-x-6">
-                        <a href="{{ route('login') }}" class="btn-primary">{{__('Login')}}</a>
-                        <a href="{{ route('register') }}" class="text-sm font-semibold leading-6 text-gray-900">{{__('Register now')}}<span aria-hidden="true">→</span></a>
+                    <div class="mt-4 flex items-center justify-around md:justify-start gap-x-6">
+                        <a href="{{ route('login') }}" class="btn-primary text-sm md:text-base">{{__('Login')}}</a>
+                        <a href="{{ route('register') }}" class="text-sm md:text-base font-semibold leading-6 text-gray-900">{{__('Register now')}}<span aria-hidden="true">→</span></a>
                     </div>
                 </div>
             </div>
-            <div class="hidden md:block md:w-2/5 pr-16 scale-in-center">
+            <div class="md:w-2/5 pr-16 scale-in-center my-6">
                 <img src="{{ asset('storage/images/camera-home.png') }}" alt="" >
             </div>
         </div>
 
-    <x-promo-welcome />
+        <x-promo-welcome />
 
-    <section id="image-carousel" class="splide my-16 md:mx-16" aria-label="Latest products">
-        <div class="mb-8 text-center">
-            <h2 class="text-2xl font-bold text-3xl">{{__('Latest products')}}
-        </div>
-        <div class="splide__track mx-8">
-            <ul class="splide__list">
-                @foreach($products as $product)
-                <li class="splide__slide border-transparent overflow-hidden rounded-lg bg-white">
-                    <a href="{{ route('product.view', $product->slug) }}"
-                       class="aspect-w-3 aspect-h-2 block">
-                        <img src="{{ $product->image }}" alt="{{$product->title}}"
-                        class="card-image object-cover hover:scale-105 hover:rotate-1 transition-transform">
-                        <div class="p-4 card-listing">
-                            <div>
-                                <h3 class="underline-hover w-fit">
-                                    {{$product->title}}
-                                </h3>
+        <section id="image-carousel" class="splide my-16 md:mx-16" aria-label="Latest products">
+            <div class="mb-8 text-center">
+                <h2 class="text-2xl font-bold text-3xl">{{__('Latest products')}}
+            </div>
+            <div class="splide__track mx-8">
+                <ul class="splide__list">
+                    @foreach($products as $product)
+                    <li class="splide__slide border-transparent overflow-hidden rounded-lg bg-white">
+                        <a href="{{ route('product.view', $product->slug) }}"
+                        class="aspect-w-3 aspect-h-2 block">
+                            <img src="{{ $product->image }}" alt="{{$product->title}}"
+                            class="card-image object-cover hover:scale-105 hover:rotate-1 transition-transform">
+                            <div class="p-4 card-listing">
+                                <div>
+                                    <h3 class="underline-hover w-fit">
+                                        {{$product->title}}
+                                    </h3>
+                                </div>
+                                <div class="price-container relative flex justify-between">
+                                    <h5 class="font-number pl-4 text-lg md:text-xl lg:text-2xl">${{$product->price}}</h5>
+                                </div>
                             </div>
-                            <div class="price-container relative flex justify-between">
-                                <h5 class="font-number pl-4 text-lg md:text-xl lg:text-2xl">${{$product->price}}</h5>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-        </div>
-    </section>
-    
-    <x-newsletter />
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </section>
+        
+        <hr class="mt-24 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
 
-</x-app-layout>
+        <x-newsletter />
+        
+    </x-app-layout>
