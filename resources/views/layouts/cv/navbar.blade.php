01c9f1b2115f703231386424c8186e8d3c6a5a67
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2024-02-05 12:57:01 +0100

    Navbar fixes

--- resources/views/layouts/cv/navbar.blade.php
+++ resources/views/layouts/cv/navbar.blade.php
@@ -41,49 +41,25 @@
             <ul>
                 <li>
                     <a
-                        href="{{ route('categories.index') }}"
+                        href="#curriculum-vitae"
                         class="relative flex items-center justify-between py-2 px-3 transition-colors underline-hover"
                     >
                         <div class="flex items-center">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5 mr-2"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="1"
-                            >
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
-                            </svg>
                             {{ __('Curriculum') }}
                         </div>
                     </a>
                 </li>
                 <li>
                     <a
-                        href="{{ route('cart.index') }}"
+                        href="#portfolio"
                         class="relative flex items-center justify-between py-2 px-3 transition-colors underline-hover"
                     >
                         <div class="flex items-center">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5 mr-2 -mt-1"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="1"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                />
-                            </svg>
                             {{ __('Portfolio') }}
                         </div>
                     </a>
                 </li>
-                <li>
+                <!-- <li>
                     <a
                         href="{{ route('login') }}"
                         class="flex items-center py-2 px-3 transition-colors underline-hover"
@@ -104,7 +80,7 @@
                         </svg>
                         {{ __('Contacto') }}
                     </a>
-                </li>
+                </li> -->
                 <li class="px-3 py-3">
                     <a
                         href="{{ route('welcome') }}"
@@ -131,9 +107,9 @@
                         <span><b>01 | </b> {{ __('Curriculum') }}</span>
                     </a>
                 </li>
-                <li class="number hidden">
+                <li class="number">
                     <a
-                        href="{{ route('cart.index') }}"
+                        href="#portfolio"
                         class="relative inline-flex items-center py-navbar-item px-navbar-item underline-hover"
                     >
                     <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
